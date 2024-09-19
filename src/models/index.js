2ff425d45e98c70e4c67c427f617fb2227dabf1b
steen <steen@lundogbendsen.dk>
2024-09-19 14:02:44 +0200

    updated model

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
 // src/models/index.js
 
-const Artist = require('./artistModel');
-const Gallery = require('./galleryModel');
-const Print = require('./printModel');
+const Artist = require('./artist');
+const Gallery = require('./gallery');
+const Print = require('./print');
 const sequelize = require('../config/database');
 const {DataTypes} = require("sequelize");
 
@@ -12,24 +12,24 @@ Artist.hasMany(Print);
 
 // Gallery associations
 Gallery.belongsTo(Artist);
-Gallery.belongsToMany(Print, { through: 'GalleryPrint' });
+Gallery.belongsToMany(Print, {through: 'GalleryPrint', as: 'prints'});
 
 // Print associations
 Print.belongsTo(Artist);
-Print.belongsToMany(Gallery, { through: 'GalleryPrint' });
+Print.belongsToMany(Gallery, {through: 'GalleryPrint', as: 'galleries'});
 
 // Define the GalleryPrint model for the many-to-many relationship
 const GalleryPrint = sequelize.define('GalleryPrint', {
-  order: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0
-  }
+    order: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+    }
 });
 
 module.exports = {
-  Artist,
-  Gallery,
-  Print,
-  GalleryPrint
+    Artist,
+    Gallery,
+    Print,
+    GalleryPrint
 };
